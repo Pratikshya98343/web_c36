3846c4ca92177dae6fbeeecbc9802259a6921161
Sandip Shrestha <c.restsandip123@gmail.com>
2025-07-06 17:09:10 +0545

    add spacing for improved readability in user route file

--- Authentication/src/route/user/userRoute.js
+++ Authentication/src/route/user/userRoute.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { userController } from "../../controller/index.js";
+
 const router = express.Router();
+
 router.get("/", userController.getAll);
 router.post("/", userController.create);
 router.put("/:id", userController.update);
