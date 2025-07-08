f5ff0ea1d42c002d21893ce5aca34efee635f3e0
oop <pratikshyashrestha94@gmail.com>
2025-07-08 08:56:14 +0545

    made changes

--- Backend/src/models/user/User.js
+++ Backend/src/models/user/User.js
@@ -1,22 +1,31 @@
 import { DataTypes } from "sequelize";
 import { sequelize } from "../../database/index.js";
 
-
-export const User=sequelize.define("User",{
+export const User = sequelize.define(
+  "User",
+  {
     id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-      },
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      password:{
-        type:DataTypes.STRING
-      }
-})
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    timestamps: true, // Automatically adds createdAt and updatedAt
+  }
+);
--- Backend/src/route/auth/authRoute.js
+++ Backend/src/route/auth/authRoute.js
@@ -3,5 +3,6 @@ import { authController } from "../../controller/index.js";
 const router = express.Router();
 router.get("/init", authController.init);
 router.post("/login", authController.login);
+router.post("/signup", authController.signup);
 
 export { router as authRouter };
